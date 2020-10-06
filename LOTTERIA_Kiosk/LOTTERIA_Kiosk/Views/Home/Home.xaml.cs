02a187ba23cfa4395b6b8a0b1a6561b36f13dd77
Name명 <>
2020-10-06 15:07:31 +0900

    [ADD] Home 화면 디자인

--- LOTTERIA_Kiosk/LOTTERIA_Kiosk/Views/Home/Home.xaml.cs
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/Views/Home/Home.xaml.cs
@@ -23,6 +23,26 @@ namespace LOTTERIA_Kiosk.Views.Home
         public HomePage()
         {
             InitializeComponent();
+
+            this.startMedia.Play();
+            this.startMedia.MediaEnded += new RoutedEventHandler(startMedia_MediaEnded);
+
+        }
+        private void startMedia_MediaEnded(object sender, RoutedEventArgs e)
+
+        {
+
+            this.startMedia.Stop();
+
+            this.startMedia.Position = TimeSpan.FromSeconds(0);
+
+            this.startMedia.Play();
+
+        }
+
+        private void OrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Order/Order.xaml", UriKind.Relative));
         }
     }
 }

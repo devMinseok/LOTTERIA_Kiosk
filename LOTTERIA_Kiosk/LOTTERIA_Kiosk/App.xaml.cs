acc795bfeb613f59c21417bf416baa1fa81070cf
DESKTOP-NI12OAL\Axe <qbq5000@gmail.com>
2020-10-14 12:20:41 +0900

    [Modify] 글로벌 변수 추가

--- LOTTERIA_Kiosk/LOTTERIA_Kiosk/App.xaml.cs
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LOTTERIA_Kiosk.DataSource;
+using LOTTERIA_Kiosk.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,24 @@ namespace LOTTERIA_Kiosk
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 주문 화면에서 선택한 메뉴 리스트
+        /// </summary>
+        public static List<Food> SelectedMenuList = new List<Food>();
+
+        /// <summary>
+        /// 좌석 데이터 소스
+        /// </summary>
+        public static SeatDataSource SeatData = new SeatDataSource();
+
+        /// <summary>
+        /// 메뉴 데이터 소스
+        /// </summary>
+        public static MenuDataSource FoodData = new MenuDataSource();
+
+        /// <summary>
+        /// 주문 정보 데이터
+        /// </summary>
+        public static List<OrderInfo> OrderInfoData = new List<OrderInfo>();
     }
 }

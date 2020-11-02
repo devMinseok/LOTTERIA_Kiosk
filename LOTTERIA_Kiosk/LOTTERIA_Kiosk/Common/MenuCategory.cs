6035b90c74680578fcd87da792905f5ebc4d3d83
DESKTOP-NI12OAL\Axe <qbq5000@gmail.com>
2020-11-02 17:20:49 +0900

    [Modify] 카테고리 열거형 수정

--- LOTTERIA_Kiosk/LOTTERIA_Kiosk/Common/MenuCategory.cs
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/Common/MenuCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,9 @@ namespace LOTTERIA_Kiosk.Common
 {
     public enum MenuCategory
     {
-        Hamburger, // 햄버거
-        Drink, // 음료수
-        Desert, // 디저트
-        Chicken // 치킨
+        햄버거,
+        음료수,
+        디저트,
+        치킨
     }
 }

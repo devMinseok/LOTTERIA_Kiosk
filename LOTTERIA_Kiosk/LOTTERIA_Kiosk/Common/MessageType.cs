0a04376e1cb8c7a33d784052a6ca1acbf173581c
DESKTOP-NI12OAL\Axe <qbq5000@gmail.com>
2020-11-09 16:56:45 +0900

    [Add] 메시지 모델 추가 + 서버에 메시지를 보내기위해 메시지 모델을 추가

---
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/Common/MessageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTTERIA_Kiosk.Common
+{
+    public enum MessageType
+    {
+        로그인 = 0,
+        일반메시지 = 1,
+        주문정보 = 2
+    }
+}
---
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/Network/RequestMessage.cs
@@ -0,0 +1,27 @@
+﻿using LOTTERIA_Kiosk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LOTTERIA_Kiosk.Network
+{
+    public class RequestMessage
+    {
+        public MessageType MSGType { get; set; }
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public string ShopName { get; set; }
+        public string OrderNumber { get; set; }
+        public List<Menu> Menus { get; set; }
+
+        public class Menu
+        {
+            public string Name { get; set; }
+            public int Count { get; set; }
+            public int Price { get; set; }
+        }
+    }
+}

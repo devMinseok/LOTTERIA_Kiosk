bc96dabfa37b7c8246fd7c8f399bb5ae226caa51
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-11-11 11:17:50 +0900

    TCP 서버 함수형으로 연결 및 json serialized 구현

--- LOTTERIA_Kiosk/LOTTERIA_Kiosk/View/Home.xaml.cs
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/View/Home.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using LOTTERIA_Kiosk.Common;
+using LOTTERIA_Kiosk.Network;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,10 +26,7 @@ namespace LOTTERIA_Kiosk.View
     /// </summary>
     public partial class Home : Page
     {
-
-        IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse("10.80.163.112"), 0);
-
-        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("10.80.162.152"), 80);
+        TCPNet tcpnet = new TCPNet();
         public Home()
         {
             InitializeComponent();
@@ -34,8 +34,20 @@ namespace LOTTERIA_Kiosk.View
             this.startMedia.Play();
             this.startMedia.MediaEnded += new RoutedEventHandler(startMedia_MediaEnded);
 
-            ConnServer();         
+            tcpnet.StartClient();
 
+            RequestMessage requestJson = new RequestMessage();
+
+            requestJson.MSGType = 0;
+            requestJson.Id = "2113";
+            requestJson.Content = "";
+            requestJson.ShopName = "";
+            requestJson.OrderNumber = "";
+            requestJson.Menus = null;
+            
+            string json = JsonConvert.SerializeObject(requestJson);
+            tcpnet.Send(json);
+            tcpnet.waitForReceive();
         }
         private void startMedia_MediaEnded(object sender, RoutedEventArgs e)
 
@@ -52,75 +64,13 @@ namespace LOTTERIA_Kiosk.View
         private void OrderBtn_Click(object sender, RoutedEventArgs e)
         {
             //NavigationService.Navigate(new Uri("/View/Order.xaml", UriKind.Relative));
-            var json = new JObject();
-            var jarray = new JArray();
-
-            json.Add("MSGType", 0);
-            json.Add("Id", "2113");
-            json.Add("Content", "");
-            json.Add("ShopName", "");
-            json.Add("OrderNumber", "");
-            json.Add("Menus", jarray);
-
-            byte[] a = Encoding.UTF8.GetBytes(json.ToString());
+            
 
         }
 
         private void Manager_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/View/Manager/Manager.xaml", UriKind.Relative));
-        }
-
-        private void ConnServer()
-        {
-            try
-            {
-
-                TcpClient client = new TcpClient(clientAddress);
-                
-
-                client.Connect(serverAddress);
-            }
-            catch
-            {
-                MessageBox.Show("예외발생!", "롯데리아");
-            }
-        }
-        private void Send(Socket client, String data)
-        {
-            var json = new JObject();
-            var jarray = new JArray();
-
-            json.Add("MSGType", 0);
-            json.Add("Id", "2113");
-            json.Add("Content", "");
-            json.Add("ShopName", "");
-            json.Add("OrderNumber", "");
-            json.Add("Menus", jarray);
-
-            byte[] a = Encoding.UTF8.GetBytes(json.ToString());
-
-            client.Send(a);
-            //client.BeginSend(a, 0, a.Length, 0, new AsyncCallback(SendCallback), client);
-        }
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.  
-                int bytesSent = client.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
-
-                // Signal that all bytes have been sent.  
-                //sendDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
         }
     }
 }

2ed01f856e02c56046b1d567753e57c5fcb8618d
DESKTOP-NI12OAL\Axe <qbq5000@gmail.com>
2020-11-29 05:19:58 +0900

    Order cs 주석추가

--- LOTTERIA_Kiosk/LOTTERIA_Kiosk/View/Order.xaml.cs
+++ LOTTERIA_Kiosk/LOTTERIA_Kiosk/View/Order.xaml.cs
@@ -45,6 +45,11 @@ namespace LOTTERIA_Kiosk.View
             LoadMenuList();
         }
 
+        /// <summary>
+        /// 메뉴 리스트박스 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lbMenus_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             Food food = lbMenus.SelectedItem as Food;
@@ -55,6 +60,11 @@ namespace LOTTERIA_Kiosk.View
             }
         }
 
+        /// <summary>
+        /// 메뉴 카테고리 리스트박스 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lbMenuCategory_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Page = 1;
@@ -63,6 +73,10 @@ namespace LOTTERIA_Kiosk.View
             SetLbMenusItem(selectedCategory);
         }
 
+        /// <summary>
+        /// 메뉴 리스트박스 아이템 설정
+        /// </summary>
+        /// <param name="category"></param>
         private void SetLbMenusItem(MenuCategory category)
         {
             FoodData = App.FoodData.foodList.FindAll(x => x.Category == category);
@@ -70,6 +84,10 @@ namespace LOTTERIA_Kiosk.View
             LoadMenuList();
         }
 
+        /// <summary>
+        /// 선택한 메뉴 추가
+        /// </summary>
+        /// <param name="food"></param>
         public void AddSelectedMenu(Food food)
         {
             if (App.SelectedMenuList.Exists(x => x.Name == food.Name))
@@ -92,6 +110,9 @@ namespace LOTTERIA_Kiosk.View
             LoadMenuList();
         }
 
+        /// <summary>
+        /// 메뉴 리스트박스 로드
+        /// </summary>
         public void LoadLbMenus()
         {
             lbMenus.Items.Clear();
@@ -110,14 +131,20 @@ namespace LOTTERIA_Kiosk.View
                 }
             }
         }
-
-    public void LoadMenuList()
+        
+        /// <summary>
+        /// 선택한 메뉴 리스트 로드
+        /// </summary>
+        public void LoadMenuList()
         {
             lvOrderList.ItemsSource = App.SelectedMenuList;
             lvOrderList.Items.Refresh();
             tbTotalPrice.Text = GetTotalPrice().ToString();
         }
-
+        
+        /// <summary>
+        /// 메뉴 카테고리 버튼 로드
+        /// </summary>
         public void LoadCategoryButton()
         {
             InitializeComponent();
@@ -125,6 +152,11 @@ namespace LOTTERIA_Kiosk.View
             SetLbMenusItem(MenuCategory.햄버거); // 초기 선택
         }
 
+        /// <summary>
+        /// 리스트뷰의 선택한 아이템을 가져오는 메서드
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
         private static Food ListView_GetItem(RoutedEventArgs e)
         {
             DependencyObject dep = (DependencyObject)e.OriginalSource;
@@ -144,6 +176,11 @@ namespace LOTTERIA_Kiosk.View
             return content;
         }
 
+        /// <summary>
+        /// 선택한 메뉴의 수량을 감소시키는 메서드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MinusMenuCount(object sender, RoutedEventArgs e)
         {
             Food food = ListView_GetItem(e);
@@ -158,6 +195,11 @@ namespace LOTTERIA_Kiosk.View
             LoadMenuList();
         }
 
+        /// <summary>
+        /// 선택한 메뉴의 수량을 증가시키는 메서드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PlusFoodCount(object sender, RoutedEventArgs e)
         {
             Food food = ListView_GetItem(e);
@@ -165,6 +207,10 @@ namespace LOTTERIA_Kiosk.View
             LoadMenuList();
         }
 
+        /// <summary>
+        /// 선택한 메뉴의 총 가격을 반환하는 메서드
+        /// </summary>
+        /// <returns></returns>
         private double GetTotalPrice()
         {
             double total = 0;
@@ -176,6 +222,11 @@ namespace LOTTERIA_Kiosk.View
             return total;
         }
 
+        /// <summary>
+        /// 선택한 메뉴 초기화
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OrderReset(object sender, RoutedEventArgs e)
         {
             if (App.SelectedMenuList.Count < 1)
@@ -198,6 +249,11 @@ namespace LOTTERIA_Kiosk.View
             }
         }
 
+        /// <summary>
+        /// 주문 취소 메서드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OrderCancel(object sender, RoutedEventArgs e)
         {
             if (MessageBox.Show("주문을 취소하시겠습니까?", "롯데리아", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
@@ -210,6 +266,11 @@ namespace LOTTERIA_Kiosk.View
             }
         }
 
+        /// <summary>
+        /// 다음 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OrderNext(object sender, RoutedEventArgs e)
         {
             if (App.SelectedMenuList.Count == 0)
@@ -221,6 +282,11 @@ namespace LOTTERIA_Kiosk.View
             }
         }
 
+        /// <summary>
+        /// 메뉴리스트 이전
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PreviousButton_Click(object sender, RoutedEventArgs e)
         {
             if (Page > 1)
@@ -230,6 +296,11 @@ namespace LOTTERIA_Kiosk.View
             }
         }
 
+        /// <summary>
+        /// 메뉴리스트 다음
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
             if (FoodData.Count >= Page * Cell)
